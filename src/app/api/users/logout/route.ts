deff28f579749dcc51cceef0316957040b051841
Ansh Jain <anshjain9159@gmail.com>
2024-05-16 21:48:00 +0530

    bugs maybe

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,11 +1,4 @@
-import {connect} from '@/dbConfig/dbCongig'
-import User from '@/models/userModel'
 import {NextRequest,NextResponse} from 'next/server'
-
-
-
-connect()
-
 
 export async function GET(){
     try {
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,23 +10,21 @@ export function middleware(request: NextRequest) {
     const token = request.cookies.get("token")?.value || ''
 
     if (isPublicPath && token) {
-        return NextResponse.redirect(new URL('/', request.url))
+        return NextResponse.redirect(new URL('/', request.nextUrl))
     }
+
     if (!isPublicPath && !token) {
-        return NextResponse.redirect(new URL('/login', request.url))
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
     }
-
-
-  return NextResponse.redirect(new URL('/home', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     '/',
+    '/profile',
     '/login',
     '/signup',
-    '/profile',
     '/verifyemail'
   ],
 }
